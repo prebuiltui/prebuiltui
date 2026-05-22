9f4ff03534709194c80f189cc38b88f2e03dc0ca
2shining <fetrafaneva@gmail.com>
2026-05-22 14:35:11 +0300

    Added RotatingGradientButton

---
+++ components/buttons/add-rotating-gradient-button/component.vue
@@ -0,0 +1,29 @@
+<template>
+  <button
+    class="gradient-button px-8 py-3 rounded-xl text-white font-medium transition duration-300 hover:shadow-2xl"
+  >
+    Read More
+  </button>
+</template>
+
+<style scoped>
+.gradient-button {
+  position: relative;
+  overflow: hidden;
+  border: none;
+  z-index: 1;
+}
+
+.gradient-button::after {
+  content: "";
+  position: absolute;
+  inset: 0;
+  z-index: -1;
+  background: linear-gradient(315deg, #1fd1f9 0%, #b621fe 74%);
+  transition: all 0.4s ease;
+}
+
+.gradient-button:hover::after {
+  transform: scale(2) rotate(180deg);
+}
+</style>
